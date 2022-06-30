11f78edd3df1770d0d1b6d3abb49a13d9d5a7510
unknown <mbisovskyi@gmail.com>
2022-06-30 12:50:20 -0400

    Project skeleton is created by following the UML

---
+++ battlefield.py
@@ -0,0 +1,17 @@
+from robot import Robot
+from dinosaur import Dinosaur
+
+class Battlefield:
+    def __init__(self):
+        def run_games(self):
+            pass
+
+        def display_welcome(self):
+            pass
+    
+        def battle_phase(self):
+            pass
+
+        def display_winner(self):
+            pass
+    
---
+++ robot.py
@@ -0,0 +1,10 @@
+from weapon import Weapon
+
+class Robot:
+    def __init__(self, name):
+        self.name = ' '
+        self.health = 100
+        self.active_weapon = Weapon()
+
+    def attack(self, dinosaur):
+        pass
